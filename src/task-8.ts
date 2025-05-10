9c3cf4809d1a10057841cc0a1f9e77c7fb8287c2
Anna Gandzulevich <gandzulevich@gmail.com>
2025-05-10 10:15:17 +0300

    correct t6 t8

--- src/task-8.ts
+++ src/task-8.ts
@@ -7,8 +7,8 @@ interface Post {
 }
 
 async function fetchPosts(): Promise<Post[]> {
-  const response = await axios.get(
-    "<https://jsonplaceholder.typicode.com/posts>"
+  const response = await axios.get<Post[]>(
+    "https://jsonplaceholder.typicode.com/posts"
   );
   return response.data;
 }
